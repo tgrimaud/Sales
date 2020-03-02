8334195ddb8ae032236664e7230024ec7b268492
Thomas Grimaud <thomas.g@aircraftms.com>
2020-03-02 09:03:54 -0500

    Add missing change in ReceiptPresenterTest

--- src/test/java/com/teksystems/sales/test/Presenter/ReceiptPresenterTest.java
+++ src/test/java/com/teksystems/sales/test/Presenter/ReceiptPresenterTest.java
@@ -22,8 +22,8 @@ public class ReceiptPresenterTest {
 	}
 	
 	@Test
-	public void testPresenter() {
-		presenter.presentReceipt(createReceipt());
+	public void testPresenter1() {
+		presenter.presentReceipt(createReceipt1());
 		
 		String expect = "1 imported bottle of perfume: 32.50\n"
 				+ "3 packet of headache pills: 9.75\n"
@@ -34,15 +34,36 @@ public class ReceiptPresenterTest {
 		
 	}
 	
+	@Test
+	public void testPresenter2() {
+		presenter.presentReceipt(createReceipt2());
+		
+		String expect = "1 imported bottle of perfume: 32.50\n"
+				+ "3 packet of headache pills: 9.80\n"
+				+ "1 box of imported chocolates: 18.29\n"
+				+ "Sales Taxes: 44.50 Total: 345.05\n";
+		
+		assertEquals(expect, strategy.getReceipt());
+		
+	}
 	
-	
-	private Receipt createReceipt() {
+	private Receipt createReceipt1() {
 		Receipt receipt = new Receipt();
 		receipt.setTotalReceipt(345.00);
 		receipt.setTotalTaxReceipt(44.50);
 		receipt.getReceiptLines().add(new ReceiptLine(1,"imported bottle of perfume", 30.50, 2.00));
 		receipt.getReceiptLines().add(new ReceiptLine(3,"packet of headache pills", 6.75, 3.00));
 		receipt.getReceiptLines().add(new ReceiptLine(1,"box of imported chocolates", 14.29, 4.00));
+		return receipt;
+	}
+	
+	private Receipt createReceipt2() {
+		Receipt receipt = new Receipt();
+		receipt.setTotalReceipt(345.0500000009);
+		receipt.setTotalTaxReceipt(44.500000002);
+		receipt.getReceiptLines().add(new ReceiptLine(1,"imported bottle of perfume", 30.50, 2.00000008));
+		receipt.getReceiptLines().add(new ReceiptLine(3,"packet of headache pills", 6.75, 3.0500009));
+		receipt.getReceiptLines().add(new ReceiptLine(1,"box of imported chocolates", 14.29, 4.000000007));
 		return receipt;
 	}
 
